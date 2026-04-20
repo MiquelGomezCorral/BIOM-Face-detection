f3368f9d1e7c05342ecdbc96adb774fd338706c3
Miquel Gómez <miquelgc2003@gmail.com>
2026-04-20 17:46:21 +0200

    paralelize testing

--- app/src/model/test.py
+++ app/src/model/test.py
@@ -5,6 +5,7 @@ from src.model import CascadeClassifier
 
 from maikol_utils.print_utils import print_separator, print_color
 from maikol_utils.file_utils import list_dir_files
+from concurrent.futures import ThreadPoolExecutor
 
 
 def test_cascade_fpr(CONFIG: Configuration, classifier: CascadeClassifier):
@@ -12,12 +13,16 @@ def test_cascade_fpr(CONFIG: Configuration, classifier: CascadeClassifier):
 
     all_crops, cn = list_dir_files(CONFIG.no_faces_crops_path)
     print(f" - Found {cn} no-face crops in {CONFIG.no_faces_crops_path}\n")
-    
+
+    def _predict_one(img_path):
+        return classifier.predict_no_merge(img_path=img_path, return_candidate_count=True)
+
     all_fps, total_candidates = [], 0
-    for img_path in tqdm(all_crops):  # Test on the first 5 images
-        fps, candidates = classifier.predict_no_merge(img_path=img_path, return_candidate_count=True)
-        all_fps.extend(fps)
-        total_candidates += candidates
+    with ThreadPoolExecutor(max_workers=CONFIG.max_cpu_cores) as ex:
+        results = ex.map(_predict_one, all_crops)
+        for fps, candidates in tqdm(results, total=len(all_crops), desc="No-faces eval"):
+            all_fps.extend(fps)
+            total_candidates += candidates
 
     fpr = len(all_fps) / total_candidates if total_candidates > 0 else 0
     print_color(f" - False Positive Rate: {fpr:.6f}", color="green")
