c98425ca014f82fa7b619b423643b2a1919717e5
Miquel Gómez <miquelgc2003@gmail.com>
2026-06-02 18:54:37 +0200

    Other best models

--- app/src/config/config.py
+++ app/src/config/config.py
@@ -35,7 +35,7 @@ class Configuration:
     
     cv_haar_cascades: str = os.path.join(MODELS_PATH, "haar_cascades")
     computed_haar_cascades: str =  os.path.join(MODELS_PATH,'haar_cascades_computed_best')
-    computed_haar_cascades_name: str = "haar_cascade_stage_21_fpr_0.0000000000_999_pfp.xml"
+    computed_haar_cascades_name: str = "haar_cascade_stage_28_fpr_0.0000016057_999_all.xml"
     computed_haar_cascades_path: str = os.path.join(computed_haar_cascades, computed_haar_cascades_name)
 
 
